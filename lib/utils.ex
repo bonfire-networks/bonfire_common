c4cf843523cbe8506c91aa145078554b2e8b9837
Mayel <git@mayel.space>
2020-11-19 19:31:15 +0100

    fix

--- lib/utils.ex
+++ lib/utils.ex
@@ -156,7 +156,7 @@ defmodule Bonfire.Common.Utils do
   Special LiveView helper function which allows loading LiveComponents in regular Phoenix views: `live_render_component(@conn, MyLiveComponent)`
   """
   def live_render_component(conn, load_live_component) do
-    if Code.ensure_loaded(load_live_component), do: live_render(@conn,
+    if Code.ensure_loaded(load_live_component), do: Phoenix.LiveView.Controller.live_render(conn,
         Bonfire.Web.LiveComponent,
         session: %{
           "load_live_component"=> load_live_component
