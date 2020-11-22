3750e65e90b475724975b45f3a7eef36b91b2de4
jjl <james.laver@gmail.com>
2020-11-22 21:17:34 +0000

    fix load session auth live plug

--- lib/web/live_plugs/load_session_auth.ex
+++ lib/web/live_plugs/load_session_auth.ex
@@ -3,20 +3,14 @@ defmodule Bonfire.Common.Web.LivePlugs.LoadSessionAuth do
   import Phoenix.LiveView
   alias Bonfire.Me.{Accounts, Users}
 
-  def mount(%{"username" => username}, %{"account_id" => account_id}, socket),
-    do: try_user(Users.get_for_session(username, account_id), account_id, socket)
-
-  def mount(_, %{"account_id" => account_id}, socket),
-    do: {:ok, assign(socket, current_user: nil, current_account: Accounts.get_for_session(account_id))}
-
-  def mount(_, _, socket), do: try_user(nil, nil, socket)
-
-  defp try_user({:ok, user}, account_id, socket) do
-    {:ok, assign(socket, current_user: user, current_account: user.accounted.account)}
+  def mount(_params, session, socket) do
+    {:ok, mount(session, assign(socket, current_user: nil, current_account: nil))}
   end
 
-  defp try_user(_, account_id, socket),
-    do: {:ok, assign(socket, current_user: nil, current_account: account_id)}
+  def mount(%{"account_id" => id}, socket) when is_binary(id) do
+    assign(socket, current_account: Accounts.get_for_session(id))
+  end
 
+  def mount(_, socket), do: socket
 
 end
