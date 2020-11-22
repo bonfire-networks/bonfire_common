9ab24e3cc588226fb72c1ecb6a23eca96fd96e58
jjl <james.laver@gmail.com>
2020-11-22 16:58:28 +0000

    fix header guest

---
+++ lib/web/live/live_component.ex
@@ -0,0 +1,36 @@
+defmodule Bonfire.Web.LiveComponent do
+  @moduledoc """
+  Special LiveView used for a helper function which allows loading LiveComponents in regular Phoenix views: `live_render_component(@conn, MyLiveComponent)`
+  """
+
+  use Bonfire.Web, :live_view
+
+  alias Bonfire.Common.Web.LivePlugs
+
+  def mount(params, session, socket) do
+    LivePlugs.live_plug params, session, socket, [
+      LivePlugs.LoadSessionAuth,
+      LivePlugs.StaticChanged,
+      LivePlugs.Csrf,
+      &mounted/3,
+    ]
+  end
+
+  defp mounted(params, %{"load_live_component" => load_live_component} = session, socket) do
+
+     {:ok, socket |> assign(:load_live_component, load_live_component)}
+  end
+
+  defp mounted(_params, _session, socket), do: {:ok, socket}
+
+  def render(assigns) do
+      ~L"""
+      <%= if Map.has_key?(assigns, :load_live_component) and Code.ensure_loaded(assigns.load_live_component), do: live_component(
+      @socket,
+      assigns.load_live_component,
+      assigns
+    ) %>
+    """
+  end
+
+end
