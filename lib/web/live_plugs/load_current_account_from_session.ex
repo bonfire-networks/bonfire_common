2502472a7206113533fd18969d862d101753a184
jjl <james.laver@gmail.com>
2020-12-17 14:56:31 +0000

    clear session in load current account liveplug when account not found

--- lib/web/live_plugs/load_current_account_from_session.ex
+++ lib/web/live_plugs/load_current_account_from_session.ex
@@ -9,7 +9,15 @@ defmodule Bonfire.Common.Web.LivePlugs.LoadCurrentAccountFromSession do
   def mount(_, %{"account_id" => id}, socket), do: check_account(Accounts.get_current(id), socket)
   def mount(_, _, socket), do: check_account(nil, socket)
 
-  defp check_account(nil, socket), do: {:halt, push_redirect(socket, to: Routes.login_path(socket, :index))}
-  defp check_account(account, socket), do: {:ok, assign(socket, current_account: account)}
+  defp check_account(%Account{}=account, socket),
+    do: {:ok, assign(socket, current_account: account)}
+
+  defp check_account(nil, socket) do
+    {:halt,
+     socket
+     |> clear_session()
+     |> push_redirect(to: Routes.login_path(socket, :index))}
+  end
+
 
 end
