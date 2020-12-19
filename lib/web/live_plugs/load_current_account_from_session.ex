5327cd99c21b5e97a837344c1d4fba072124d816
Mayel <git@mayel.space>
2020-12-19 13:11:30 +0100

    signup flow fixes

--- lib/web/live_plugs/load_current_account_from_session.ex
+++ lib/web/live_plugs/load_current_account_from_session.ex
@@ -5,11 +5,12 @@ defmodule Bonfire.Common.Web.LivePlugs.LoadCurrentAccountFromSession do
   alias Bonfire.Data.Identity.Account
 
   # the non-live plug already supplied the current account
-  def mount(_, _, %{assigns: %{current_account: %Account{}}}=socket), do: {:ok, socket}
-  def mount(_, %{"account_id" => id}, socket), do: check_account(Accounts.get_current(id), socket)
+  def mount(_, _, %{assigns: %{current_account: %Account{}}}=socket), do: {:ok, socket} #|> IO.inspect
+  def mount(_, %{"account_id" => id}, socket), do: check_account(Accounts.get_current(id), socket) #|> IO.inspect
   def mount(_, _, socket), do: check_account(nil, socket)
 
-  defp check_account({:ok, account}, socket), do: {:ok, assign(socket, current_account: account)}
+  defp check_account(%Account{}=account, socket), do: {:ok, assign(socket, current_account: account)}
+  # defp check_account(a, socket), do: a |> IO.inspect
   defp check_account(_, socket), do: {:halt, push_redirect(socket, to: Routes.login_path(socket, :index))}
 
 end
