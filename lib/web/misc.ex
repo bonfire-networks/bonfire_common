52b05f68dc69544663cbea8aad12848964b330f5
jjl <james.laver@gmail.com>
2020-12-22 14:51:40 +0000

    B.C.Web.Misc for 'go' redirects

---
+++ lib/web/misc.ex
@@ -0,0 +1,22 @@
+defmodule Bonfire.Common.Web.Misc do
+
+  # copies the 'go' part of the query string, if any
+  def copy_go(%{go: go}), do: "?" <> Query.encode(go: go)
+  def copy_go(%{"go" => go}), do: "?" <> Query.encode(go: go)
+  def copy_go(_), do: ""
+  
+  # TODO: should we preserve query strings?
+  def go_query(conn), do: "?" <> Query.encode(go: conn.requested_path)
+
+  # TODO: we should validate this a bit harder. Phoenix will prevent
+  # us from sending the user to an external URL, but it'll do so by
+  # means of a 500 error.
+  def valid_go_path?("/" <> _), do: true
+  def valid_go_path?(_), do: false 
+
+  def go_where?(conn, params, default) do
+    go = params[:go] || params["go"]
+    if valid_go_path?(go), do: go, else: default
+  end
+
+end
