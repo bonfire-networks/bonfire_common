4337d289d8b345cb018ee290fda761a3ea553078
Mayel <git@mayel.space>
2020-12-26 12:28:54 +0100

    fixes

--- lib/web/live_plugs/load_current_user.ex
+++ lib/web/live_plugs/load_current_user.ex
@@ -2,7 +2,7 @@ defmodule Bonfire.Common.Web.LivePlugs.LoadCurrentUser do
 
   use Bonfire.Web, :live_plug
   alias Bonfire.Me.Identity.{Accounts, Users}
-  alias Bonfire.Me.Web.SwitchUserLive
+  # alias Bonfire.Me.Web.SwitchUserLive
   alias Bonfire.Data.Identity.User
 
   # the non-live plug already supplied the current user
@@ -11,10 +11,15 @@ defmodule Bonfire.Common.Web.LivePlugs.LoadCurrentUser do
   end
 
   def mount(_, %{"user_id" => id}, socket) do
-    current_user = Users.get_current(id, Map.get(socket.assigns, :current_account))
-    # IO.inspect(current_user: current_user)
+    current_user = Users.get_current(id)
+    IO.inspect(current_user: current_user)
     check_user(current_user, socket)
   end
+
+  # def mount(a, b, socket) do
+  #   IO.inspect(a)
+  #   IO.inspect(b)
+  # end
 
   def mount(_, _, socket), do: check_user(nil, socket)
 
@@ -24,7 +29,8 @@ defmodule Bonfire.Common.Web.LivePlugs.LoadCurrentUser do
     {:ok, assign(socket, current_user: user)}
   end
 
-  defp check_user(_, socket) do
+  defp check_user(user, socket) do
+    IO.inspect(check_user: user)
     path = Routes.switch_user_path(socket, :index)
     {:halt, push_redirect(socket, to: path)}
   end
--- lib/web/misc.ex
+++ lib/web/misc.ex
@@ -4,17 +4,22 @@ defmodule Bonfire.Common.Web.Misc do
   def copy_go(%{go: go}), do: "?" <> Query.encode(go: go)
   def copy_go(%{"go" => go}), do: "?" <> Query.encode(go: go)
   def copy_go(_), do: ""
-  
+
   # TODO: should we preserve query strings?
-  def go_query(conn), do: "?" <> Query.encode(go: conn.requested_path)
+  def go_query(%{requested_path: requested_path}=_conn), do: "?" <> Query.encode(go: requested_path)
+  def go_query(_conn), do: ""
 
   # TODO: we should validate this a bit harder. Phoenix will prevent
   # us from sending the user to an external URL, but it'll do so by
   # means of a 500 error.
   def valid_go_path?("/" <> _), do: true
-  def valid_go_path?(_), do: false 
+  def valid_go_path?(_), do: false
 
-  def go_where?(conn, params, default) do
+  def go_where?(_conn, %Ecto.Changeset{}, default) do
+    default
+  end
+
+  def go_where?(_conn, params, default) do
     go = params[:go] || params["go"]
     if valid_go_path?(go), do: go, else: default
   end
