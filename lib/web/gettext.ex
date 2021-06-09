0a2ccb65e6b9091b631227025f2cac51bdb7fa7c
Mayel <git@mayel.space>
2021-06-09 13:41:26 +0200

    localise

--- lib/web/gettext.ex
+++ lib/web/gettext.ex
@@ -1,26 +1,4 @@
 defmodule Bonfire.Web.Gettext do
-  @moduledoc """
-  A module providing Internationalization with a gettext-based API.
-
-  By using [Gettext](https://hexdocs.pm/gettext),
-  your module gains a set of macros for translations, for example:
-
-      import Bonfire.Web.Gettext
-
-      # Simple translation
-      gettext("Here is the string to translate")
-
-      # Plural translation
-      ngettext("Here is the string to translate",
-               "Here are the strings to translate",
-               3)
-
-      # Domain-based translation
-      dgettext("errors", "Here is the error message to translate")
-
-  See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
-  """
-
   @default_locale "en" # TODO: configurable?
 
   use Gettext,
@@ -30,56 +8,151 @@ defmodule Bonfire.Web.Gettext do
 
   def default_locale, do: @default_locale
 
-
 end
 defmodule Bonfire.Web.Gettext.Helpers do
+  @moduledoc """
+  A module providing Internationalization with a gettext-based API.
+
+  By using [Gettext](https://hexdocs.pm/gettext),
+  your module gains a set of macros for translations, for example:
+
+      import Bonfire.Web.Gettext
+
+      # Simple translation
+
+      usage:
+        <%= l("Hello") %>
+        <%= l("Hello %{name}", name: "Bookchin") %>
+        <%= l("Hi", [], "test context") %>
+
+
+      output:
+        Hello
+        Hello Bookchin
+        Hi
+
+
+      # Plural translation
+
+      usage:
+        <%= lp("Hi friend", "Hi friends", 2) %>
+        <%= lp("Hiya %{user_or_users}", "Hiyas %{user_or_users}", 1, [user_or_users: "Bookchin"], "test context") %>
+
+      output:
+        Hi friends
+        Hiya Bookchin
+
+  See the [Gettext Docs](https://hexdocs.pm/gettext) for details.
+  """
+
   # alias the gettext macros for ease-of-use
 
   import Bonfire.Web.Gettext
 
-  defmacro l(original_text_or_id, bindings \\ nil, context \\ nil, domain \\ nil)
+  defmacro l(original_text_or_id, bindings \\ [], context \\ nil, domain \\ nil)
 
-  defmacro l(msgid, bindings, nil, nil) do
+  defmacro l(msgid, bindings, nil, nil) when is_list(bindings) or is_map(bindings) and is_binary(msgid) do
     # IO.inspect(__CALLER__)
     mod = __CALLER__.module
     case Application.get_application(mod) do
 
       otp_app when is_atom(otp_app) and not is_nil(otp_app) ->
 
-        IO.inspect(msgid, label: "based on module #{inspect mod} of #{inspect otp_app}")
+        IO.inspect(msgid, label: "domain based on module #{inspect mod}: #{inspect otp_app}")
 
-        quote do: dgettext(unquote(Atom.to_string(otp_app)), unquote(msgid), unquote(bindings) || %{})
+        quote do: dgettext(unquote(Atom.to_string(otp_app)), unquote(msgid), unquote(bindings))
 
       _ ->
+
         IO.inspect(msgid, label: "no domain or context / #{inspect mod}")
 
-        quote do: gettext(unquote(msgid), unquote(bindings) || %{})
+        quote do: gettext(unquote(msgid), unquote(bindings))
     end
   end
 
-  # defmacro l(msgid, bindings, context, nil) do
-  #   case Application.get_application(__CALLER__) do
-  #     otp_app when is_atom(otp_app) -> dngettext(otp_app, context, msgid, bindings || %{}) |> IO.inspect(label: "context, based on module")
-  #     _ -> pgettext(context, msgid, bindings || %{}) |> IO.inspect(label: "custom context, no domain")
-  #   end
-  # end
+  defmacro l(msgid, bindings, context, nil) when is_binary(context) and is_list(bindings) or is_map(bindings) and is_binary(msgid) do
+    case Application.get_application(__CALLER__.module) do
+      otp_app when is_atom(otp_app) and not is_nil(otp_app) ->
 
-  # defmacro l(msgid, bindings, nil, domain) do
-  #   dgettext(domain, msgid, bindings || %{}) |> IO.inspect(label: "custom domain")
-  # end
+        IO.inspect(msgid, label: "custom context #{context} + domain based on module #{inspect otp_app}")
 
-  # defmacro l(msgid, bindings, context, domain) do
-  #    dngettext(otp_app, context, msgid, bindings || %{}) |> IO.inspect(label: "custom context+domain")
-  # end
+        quote do: dpgettext(unquote(Atom.to_string(otp_app)), unquote(context), unquote(msgid), unquote(bindings))
+
+      _ ->
+
+        IO.inspect(msgid, label: "custom context #{context} - no domain")
+
+        quote do: pgettext(unquote(context), unquote(msgid), unquote(bindings))
+    end
+  end
+
+  defmacro l(msgid, bindings, nil, domain) when is_binary(domain) and is_list(bindings) or is_map(bindings) and is_binary(msgid) do
+    IO.inspect(msgid, label: "custom domain #{domain}")
+
+    quote do: dgettext(unquote(domain), unquote(msgid), unquote(bindings))
+  end
+
+  defmacro l(msgid, bindings, context, domain) when is_binary(domain) and is_binary(context) and is_list(bindings) or is_map(bindings) and is_binary(msgid) do
+
+    IO.inspect(msgid, label: "custom context #{context} + domain #{domain}")
+
+    quote do: dpgettext(unquote(domain), unquote(context), unquote(msgid), unquote(bindings))
+  end
 
 
-  # defmacro ln(msgid, msgid_plural, n, bindings \\ %{}), do: ngettext(msgid, msgid_plural, n, bindings) # pluralised
-  # defmacro lnc(msgctxt, msgid, msgid_plural, n, bindings \\ %{}), do: pngettext(msgctxt, msgid, msgid_plural, n, bindings) # pluralised+context
-  # defmacro lnd(domain, msgid, msgid_plural, n, bindings \\ %{}), do: dngettext(domain, msgid, msgid_plural, n, bindings) # pluralised+domain
-  # defmacro lndc(domain, msgctxt, msgid, msgid_plural, n, bindings \\ %{}), do: dngettext(domain, msgctxt, msgid, msgid_plural, n, bindings) # pluralised+domain+context
+  ### Localisation with pluralisation ###
 
-  # defmacro ld(domain, msgid, bindings \\ %{}), do: dgettext(domain, msgid, bindings) # with domain other than default
-  # defmacro lc(msgctxt, msgid, bindings \\ %{}), do: pgettext(msgctxt, msgid, bindings) # with context
-  # defmacro ldc(domain, msgctxt, msgid, bindings \\ %{}), do: dngettext(domain, msgctxt, msgid, bindings) # with comain+context
+  defmacro lp(original_text_or_id, msgid_plural, n, bindings \\ [], context \\ nil, domain \\ nil)
+
+  defmacro lp(msgid, msgid_plural, n, bindings, nil, nil) when is_binary(msgid) and is_binary(msgid_plural) and not is_nil(n) and is_list(bindings) or is_map(bindings) do
+    # IO.inspect(__CALLER__)
+    mod = __CALLER__.module
+    case Application.get_application(mod) do
+
+      otp_app when is_atom(otp_app) and not is_nil(otp_app) ->
+
+        IO.inspect(msgid, label: "plural: domain based on module #{inspect mod}: #{inspect otp_app}")
+
+        quote do: dngettext(unquote(Atom.to_string(otp_app)), unquote(msgid), unquote(msgid_plural), unquote(n), unquote(bindings))
+
+      _ ->
+
+        IO.inspect(msgid, label: "plural: no domain or context / #{inspect mod}")
+
+        quote do: ngettext(unquote(msgid), unquote(msgid_plural), unquote(n), unquote(bindings))
+    end
+  end
+
+  defmacro lp(msgid, msgid_plural, n, bindings, context, nil)  when is_binary(msgid) and is_binary(msgid_plural) and not is_nil(n) and is_list(bindings) or is_map(bindings) and is_binary(context) do
+    case Application.get_application(__CALLER__.module) do
+      otp_app when is_atom(otp_app) and not is_nil(otp_app) ->
+
+        IO.inspect(msgid, label: "plural: custom context #{context} + domain based on module #{inspect otp_app}")
+
+        quote do: dpngettext(unquote(Atom.to_string(otp_app)), unquote(context), unquote(msgid), unquote(msgid_plural), unquote(n), unquote(bindings))
+
+      _ ->
+
+        IO.inspect(msgid, label: "plural: custom context #{context} - no domain")
+
+        quote do: pngettext(unquote(context), unquote(msgid), unquote(msgid_plural), unquote(n), unquote(bindings))
+    end
+  end
+
+  defmacro lp(msgid, msgid_plural, n, bindings, nil, domain) when is_binary(msgid) and is_binary(msgid_plural) and not is_nil(n) and is_list(bindings) or is_map(bindings) and is_binary(domain)  do
+
+    IO.inspect(msgid, label: "plural: custom domain #{domain}")
+
+    quote do: dngettext(unquote(domain), unquote(msgid), unquote(msgid_plural), unquote(n), unquote(bindings))
+  end
+
+  defmacro lp(msgid, msgid_plural, n, bindings, context, domain) when is_binary(msgid) and is_binary(msgid_plural) and not is_nil(n) and is_list(bindings) or is_map(bindings) and is_binary(context) and is_binary(domain)  do
+
+    IO.inspect(msgid, label: "plural: custom context #{context} + domain #{domain}")
+
+    quote do: dpngettext(unquote(domain), unquote(context), unquote(msgid), unquote(msgid_plural), unquote(n), unquote(bindings))
+  end
+
+
 
 end
