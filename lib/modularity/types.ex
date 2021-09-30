e28650e2e85d4cf2b18e86bd2f6941b5edbb037e
Mayel <git@mayel.space>
2021-09-30 15:34:36 +1300

    type for AP

--- lib/modularity/types.ex
+++ lib/modularity/types.ex
@@ -1,11 +1,12 @@
 defmodule Bonfire.Common.Types do
   alias Bonfire.Common.Utils
 
-  def object_type(%{__struct__: schema}) when schema !=Pointers.Pointer, do: object_type(schema)
   def object_type(%{__typename: type}), do: object_type(type) # for graphql queries
   def object_type(%{table_id: type}), do: object_type(type) # for schema-less queries
+  def object_type(%{pointer_id: type}), do: object_type(type) # for AP objects
   def object_type(%{index_type: type}), do: object_type(Utils.maybe_str_to_atom(type)) # for search results
   def object_type(%{object: object}), do: object_type(object) # for activities
+  def object_type(%{__struct__: schema}) when schema !=Pointers.Pointer, do: object_type(schema)
 
   # TODO: make config-driven or auto-generate by code (eg. TypeService?)
 
