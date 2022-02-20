95d2bc891399891cbe019784629606997b3e0a67
Mayel de Borniol <git@mayel.space>
2022-02-21 12:32:49 +1300

    fix

--- lib/modularity/extend.ex
+++ lib/modularity/extend.ex
@@ -95,12 +95,12 @@ defmodule Bonfire.Common.Extend do
   def quoted_use_if_enabled({_, _, _} = module_name_ast, fallback_module), do: quoted_use_if_enabled(module_name_ast |> Macro.to_string() |> Utils.maybe_str_to_module(), fallback_module)
   def quoted_use_if_enabled(module, fallback_module) do
     if is_atom(module) and module_enabled?(module) do
-      # debug("Found module to use: #{module}")
+      debug(module, "Found module to use")
       quote do
         use unquote(module)
       end
     else
-      debug("Did not find module to use: #{inspect module}")
+      warn(module, "Did not find module to use")
       if is_atom(fallback_module) and module_enabled?(fallback_module) do
         quote do
           use unquote(fallback_module)
@@ -114,12 +114,12 @@ defmodule Bonfire.Common.Extend do
   def quoted_import_if_enabled({_, _, _} = module_name_ast, fallback_module), do: quoted_import_if_enabled(module_name_ast |> Macro.to_string() |> Utils.maybe_str_to_module(), fallback_module)
   def quoted_import_if_enabled(module, fallback_module \\ nil) do
     if is_atom(module) and module_enabled?(module) do
-      # debug("Found module to import: #{module}")
+      debug(module, "Found module to import")
       quote do
         import unquote(module)
       end
     else
-      debug("Did not find module to import: #{inspect module}")
+      warn(module, "Did not find module to import")
       if is_atom(fallback_module) and module_enabled?(fallback_module) do
         quote do
           import unquote(fallback_module)
@@ -133,12 +133,12 @@ defmodule Bonfire.Common.Extend do
   def quoted_require_if_enabled({_, _, _} = module_name_ast, fallback_module), do: quoted_require_if_enabled(module_name_ast |> Macro.to_string() |> IO.inspect |> Utils.maybe_str_to_module() |> IO.inspect, fallback_module)
   def quoted_require_if_enabled(module, fallback_module \\ nil) do
     if is_atom(module) and module_enabled?(module) do
-      # debug("Found module to require: #{module}")
+      debug(module, "Found module to require")
       quote do
         require unquote(module)
       end
     else
-      debug("Did not find module to require: #{inspect module}")
+      warn(module, "Did not find module to require")
       if is_atom(fallback_module) and module_enabled?(fallback_module) do
         quote do
           require unquote(fallback_module)
