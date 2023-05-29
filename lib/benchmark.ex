68c37c8b0c82ed3e93c777830a3626323d76044b
Mayel de Borniol <git@mayel.space>
2023-05-29 12:33:02 +0100

    bench

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -5,7 +5,7 @@ defmodule Bonfire.Common.Benchmark do
   def apply_timed(function) do
     {time, result} = :timer.tc(function)
 
-    info("Time to run anonymous function #{inspect(function)}: #{time / 1_000} ms")
+    IO.inspect("Time to run anonymous function #{inspect(function)}: #{time / 1_000} ms")
 
     result
   end
@@ -13,7 +13,7 @@ defmodule Bonfire.Common.Benchmark do
   def apply_timed(function, args) do
     {time, result} = :timer.tc(function, args)
 
-    info("Time to run #{inspect(function)}/#{length(args)}: #{time / 1_000} ms")
+    IO.inspect("Time to run #{inspect(function)}/#{length(args)}: #{time / 1_000} ms")
 
     result
   end
@@ -21,7 +21,7 @@ defmodule Bonfire.Common.Benchmark do
   def apply_timed(module, function, args) do
     {time, result} = :timer.tc(module, function, args)
 
-    info("Time to run #{module}.#{function}/#{length(args)}: #{time / 1_000} ms")
+    IO.inspect("Time to run #{module}.#{function}/#{length(args)}: #{time / 1_000} ms")
 
     result
   end
