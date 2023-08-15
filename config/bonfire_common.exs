345f68cd69c31c2652f2edead406905c7cf103de
Mayel de Borniol <git@mayel.space>
2023-08-15 20:01:32 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/620

--- config/bonfire_common.exs
+++ config/bonfire_common.exs
@@ -16,7 +16,7 @@ config :bonfire_common, Bonfire.Common.Localise.Cldr,
   default_locale: default_locale,
   # locales that will be made available on top of those for which gettext localisation files are available
   locales: ["fr", "en", "es"],
-  providers: [Cldr.Language, Cldr.DateTime, Cldr.Number, Cldr.Calendar],
+  providers: [Cldr.Language, Cldr.DateTime, Cldr.Number, Cldr.Unit, Cldr.List, Cldr.Calendar],
   gettext: Bonfire.Common.Localise.Gettext,
   # extra Gettex modules from dependencies not using the one from Bonfire.Common, so we can change their locale too
   extra_gettext: [Timex.Gettext],
@@ -26,6 +26,9 @@ config :bonfire_common, Bonfire.Common.Localise.Cldr,
   # precompile_transliterations: [{:latn, :arab}, {:thai, :latn}]
   force_locale_download: Mix.env() == :prod,
   generate_docs: true
+
+config :ex_cldr_units,
+  default_backend: Bonfire.Common.Localise.Cldr
 
 config :ex_cldr,
   default_locale: default_locale,
