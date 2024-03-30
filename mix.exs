10732caed4b321cfef44056a2d6d3bbe26c23a65
Mayel de Borniol <git@mayel.space>
2024-03-30 23:33:20 +0000

    up

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Bonfire.Common.MixProject do
             {:zest, "~> 0.1", optional: true},
             {:sentry, "~> 10.0", optional: true},
             {:dataloader, "~> 2.0", optional: true},
-            {:floki, "~> 0.33.1", optional: true},
+            {:floki, "~> 0.36", optional: true},
             {:emote,
              git: "https://github.com/bonfire-networks/emote", branch: "master", optional: true},
             {:text, "~> 0.2.0", optional: true},
