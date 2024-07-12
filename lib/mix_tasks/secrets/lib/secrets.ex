f983cc7b0f38257bbd2fc8ec5b0c1655a01c72f4
Mayel de Borniol <git@mayel.space>
2024-07-12 10:56:03 +0100

    write secrets to .env

--- lib/mix_tasks/secrets/lib/secrets.ex
+++ lib/mix_tasks/secrets/lib/secrets.ex
@@ -11,14 +11,17 @@ defmodule Mix.Tasks.Bonfire.Secrets do
 
   # for running as escript
   def main(args) do
+    # IO.inspect(args)
     run(args)
   end
 
   @doc false
   def run([]), do: run(["64"])
 
+  def run(["--file", path]), do: update_env_secrets(path)
+
   def run([int]),
-    do: int |> parse!() |> random_string() |> Kernel.<>("\r\n") |> IO.puts()
+    do: int |> parse!() |> random_string() |> print()
 
   def run([int, iterate]), do: for(_ <- 1..parse!(iterate), do: run([int]))
   def run(args), do: invalid_args!(args)
@@ -29,6 +32,9 @@ defmodule Mix.Tasks.Bonfire.Secrets do
       _ -> invalid_args!(int)
     end
   end
+
+  def print(int),
+    do: "\r\n#{int}" |> IO.puts()
 
   defp random_string(length) when length > 31 do
     :crypto.strong_rand_bytes(length)
@@ -41,5 +47,63 @@ defmodule Mix.Tasks.Bonfire.Secrets do
 
   defp invalid_args!(args) do
     raise "Expected a length as integer or no argument at all, got #{inspect(args)}"
+  end
+
+  @secret_regexes [
+    ~r/^(SECRET_KEY_BASE=)(.*)$/m,
+    ~r/^(SIGNING_SALT=)(.*)$/m,
+    ~r/^(ENCRYPTION_SALT=)(.*)$/m
+  ]
+
+  @pw_regexes [
+    {"Postgres DB password", ~r/^(POSTGRES_PASSWORD=)(.*)$/m},
+    {"MeiliSearch password", ~r/^(MEILI_MASTER_KEY=)(.*)$/m}
+  ]
+
+  defp update_env_secrets(file_path) do
+    with {:ok, content} <- File.read(file_path) do
+      new_content =
+        Enum.reduce(@secret_regexes, content, fn regex, acc ->
+          secret = random_string(64)
+          print(secret)
+          String.replace(acc, regex, "\\1#{secret}")
+        end)
+
+      content =
+        if confirm_update(
+             "the Phoenix secret keybase, signing salt, and encryption salt with the above in #{file_path}"
+           ) do
+          new_content
+        else
+          content
+        end
+
+      content =
+        Enum.reduce(@pw_regexes, content, fn {detail, regex}, acc ->
+          secret = random_string(32)
+          print(secret)
+
+          if confirm_update(
+               "the #{detail} with the above in #{file_path} (NOTE: if using docker and you've already started the container before, this will not update the server's password and cause the connection to fail)"
+             ) do
+            String.replace(acc, regex, "\\1#{secret}")
+          else
+            acc
+          end
+        end)
+
+      File.write(file_path, content)
+    else
+      e -> IO.inspect(e)
+    end
+  end
+
+  defp confirm_update(details) do
+    IO.puts("\r\nDo you want to replace #{details}? (y/n)")
+
+    case IO.gets("") |> String.trim() |> String.downcase() do
+      "y" -> true
+      _ -> false
+    end
   end
 end
